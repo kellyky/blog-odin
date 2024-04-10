846f3ff93909d3ed2fdc8396e4709dd3e13d5e26
Kelly <greenglittergypsy@gmail.com>
2024-04-09 20:53:59 -0500

    Add edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    params.require(:article).permit(:title, :body)
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
